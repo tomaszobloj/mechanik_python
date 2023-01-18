27f469435da6ec7e78bdfe8b78073082e0e4059f
Tomasz Obłój <61419484+tomaszobloj@users.noreply.github.com>
2023-01-18 15:21:21 +0000

    zapisywanie czasu logowania do pliku

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ Zapisywanie obiektów w liscie.
 '''
 
 import dane_logowania
+import datetime
+
 
 # system logowania, przyjmuje nazwe użytkownika i hasło
 def system_logowania(inlog, inpass):
-    if inlog == dane_logowania.username:
-      if inpass == dane_logowania.password:
-        print("Zalogowano!")
-      else:
-        print("Złe dane logowania!")
+  if inlog == dane_logowania.username:
+    if inpass == dane_logowania.password:
+      print("Zalogowano!")
+      zapisywanie_czasu()
     else:
-      print("Nie ma takiego użytkownika")
+      print("Złe dane logowania!")
+  else:
+    print("Nie ma takiego użytkownika")
+
 
 # wprowadzanie nazwy użytkownika
 def wprowadz_nazwe():
@@ -23,10 +27,20 @@ def wprowadz_nazwe():
   input_login = input("Podaj nazwę użytkownika: ")
   return input_login
 
+
 # wprowadzanie hasła użytkownika
 def wprowadz_haslo():
   input_password = input("Podaj hasło: ")
   print("==========")
   return input_password
+
+
+# zapisuje w pliku czas kiedy użytkownik loguje sie do systemu
+def zapisywanie_czasu():
+  czas = datetime.datetime.now()
+  f = open("czas_logowania.txt", "+a")
+  f.write(str(czas))
+  f.close()
+
 
 system_logowania(wprowadz_nazwe(), wprowadz_haslo())
