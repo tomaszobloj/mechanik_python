791b1786f61276e974bd0d792e8ba34dcfd2735f
Tomasz Obłój <61419484+tomaszobloj@users.noreply.github.com>
2023-01-18 15:11:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+'''
+System logowania z dekoratorem ktory zapisuje dane logowaniaw pliku.
+Program obiektowy(klasy, konstruktory, destruktory, hermetyzacja).
+Wykorzysywanie modułów(import...).
+Zapisywanie obiektów w liscie.
+'''
