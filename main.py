5ee2c2b2e20b35e4a77842a42b4b4d0675ceb9f2
Tomasz Obłój <61419484+tomaszobloj@users.noreply.github.com>
2023-01-18 15:18:26 +0000

    system logowania

--- main.py
+++ main.py
@@ -4,3 +4,29 @@ Program obiektowy(klasy, konstruktory, destruktory, hermetyzacja).
 Wykorzysywanie modułów(import...).
 Zapisywanie obiektów w liscie.
 '''
+
+import dane_logowania
+
+# system logowania, przyjmuje nazwe użytkownika i hasło
+def system_logowania(inlog, inpass):
+    if inlog == dane_logowania.username:
+      if inpass == dane_logowania.password:
+        print("Zalogowano!")
+      else:
+        print("Złe dane logowania!")
+    else:
+      print("Nie ma takiego użytkownika")
+
+# wprowadzanie nazwy użytkownika
+def wprowadz_nazwe():
+  print("==========")
+  input_login = input("Podaj nazwę użytkownika: ")
+  return input_login
+
+# wprowadzanie hasła użytkownika
+def wprowadz_haslo():
+  input_password = input("Podaj hasło: ")
+  print("==========")
+  return input_password
+
+system_logowania(wprowadz_nazwe(), wprowadz_haslo())
